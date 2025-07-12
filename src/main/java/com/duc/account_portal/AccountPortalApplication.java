5ae403cb91b57565070a5faf295f7a9dec725a74
ducvn <ducvn.dev@gmail.com>
2025-07-12 15:30:50 +0700

    add template source

---
+++ src/main/java/com/duc/account_portal/AccountPortalApplication.java
@@ -0,0 +1,13 @@
+package com.duc.account_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountPortalApplication.class, args);
+	}
+
+}
